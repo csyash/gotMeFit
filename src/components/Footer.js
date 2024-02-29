111d0a2863a6972b7993bd39a0060373d6405147
csyash <ykhemani2@gmail.com>
2024-02-29 17:22:56 +0530

    updated readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    <Stack
+      gap="40px"
+      sx={{ alignItems: "center" }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <Typography
+        variant="h3"
+        sx={{ fontSize: { lg: "40px", xs: "28px" } }}
+        color={"red"}
+        fontWeight={"700"}
+      >
+        @Got Me Fit
+      </Typography>
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+      mt="41px"
+      textAlign="center"
+      pb="40px"
+    >
+      Made by YASH
+    </Typography>
   </Box>
 );
 
